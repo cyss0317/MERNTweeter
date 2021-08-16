fbe808f255d8c3d1f2ec1b38995136f4c9ffd6b3
Yun Sung Choi <cyss0317@gmail.com>
2021-08-16 11:14:44 -0500

    Pushing MernTweeter

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-// mongodb + srv://admin:asdfasdf @mern.my4wh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+app.use(passport.initialize());
 
-app.get("/", (req, res)=> {
-    res.send("Heasdfsalloadsfgasgsw WahbodiOrld!");
-})
+mongoose
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Connected to mongoDB") )
+.catch( err => console.log(err));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// require('./config/passport')(passport);
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
